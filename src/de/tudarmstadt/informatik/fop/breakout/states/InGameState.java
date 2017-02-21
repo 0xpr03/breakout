a4435a4108db09d2dce537697b007a48f6a563f6
Aron Heinecke <aron.heinecke@t-online.de>
2017-02-21 05:02:08 +0100

    Added escape screen

--- src/de/tudarmstadt/informatik/fop/breakout/states/InGameState.java
+++ src/de/tudarmstadt/informatik/fop/breakout/states/InGameState.java
@@ -3,6 +3,7 @@ package de.tudarmstadt.informatik.fop.breakout.states;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
@@ -28,6 +29,12 @@ public class InGameState extends GameState {
 
 	private Logger logger = LogManager.getLogger(this);
 
+	private Button bResume;
+	private Button bMainScreen;
+	private Background bPaused;
+	
+	boolean isPaused = false;
+	
 	/**
 	 * Creates a new instance of MainMenuState
 	 * 
@@ -37,19 +44,51 @@ public class InGameState extends GameState {
 	public InGameState(int sid) {
 		stateID = sid;
 	}
+	
+	@Override
+	public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+		super.enter(container, game);
+		this.isPaused = false;
+	}
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		logger.entry();
-		objects.add(new Background(new Image("images/pause.png"), container));
 
 		objects.add(new Stick(new Vector2f(400,550), 60, 20, new Image("images/stick.png")));
 		
 		objects.add(new Ball(new Vector2f(400,500), 50, 50, new Image("images/ball.png")));
 		
 		objects.add(new Block(new Vector2f(100,100), 50, 20, new Image("images/block_1.png")));
+		
+		bPaused = new Background(new Image("images/pause.png"), container);
+		bResume = new Button(new Vector2f(400, 100), 60, 10, new Image("images/stick.png"), new Image("images/stick.png"), new ButtonAction() {
+			
+			@Override
+			public void action(GameContainer container, StateBasedGame game, GameState state, int delta) {
+				logger.trace("Resume pressed");
+				isPaused = false;
+			}
+		});
+		bMainScreen = new Button(new Vector2f(400, 200), 60, 10, new Image("images/stick.png"), new Image("images/stick.png"), new ButtonAction() {
+			@Override
+			public void action(GameContainer container, StateBasedGame game, GameState state, int delta) {
+				logger.trace("Back to MainMenu pressed");
+				game.enterState(GameParameters.MAINMENU_STATE);
+			}
+		});
 
 		logger.exit();
+	}
+	
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		super.render(container, game, g);
+		if(isPaused){
+			bPaused.render(g);
+			bResume.render(g);
+			bMainScreen.render(g);
+		}
 	}
 
 	@Override
@@ -62,6 +101,10 @@ public class InGameState extends GameState {
 		super.update(container, game, delta);
 		Input input = container.getInput();
 		if(input.isKeyPressed(Input.KEY_ESCAPE))
-			game.enterState(GameParameters.MAINMENU_STATE);
+			isPaused = !isPaused;
+		if(isPaused){
+			bMainScreen.update(container, game, this, delta);
+			bResume.update(container, game, this, delta);
+		}
 	}
 }
