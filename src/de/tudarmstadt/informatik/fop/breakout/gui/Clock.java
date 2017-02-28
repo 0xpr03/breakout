a0ec874f966dba5831542f5b676b368955e732a2
FLOP4Guido <s.kohaut@t-online.de>
2017-02-28 15:57:31 +0100

    JavaDoc

--- src/de/tudarmstadt/informatik/fop/breakout/gui/Clock.java
+++ src/de/tudarmstadt/informatik/fop/breakout/gui/Clock.java
@@ -10,15 +10,26 @@ import org.newdawn.slick.state.StateBasedGame;
 import de.tudarmstadt.informatik.fop.breakout.states.GameState;
 import gameObjects.GameObject;
 
+/**
+ * A Clock to display the elapsed time on screen
+ * 
+ * @author Simon Kohaut
+ */
 public class Clock extends Label {
 
 	private DecimalFormat timeFormat = new DecimalFormat("#.##");
 	private float timePassed;
-	
+
+	/**
+	 * Create a new Clock instance
+	 * 
+	 * @param position
+	 *            The position where the Clock will be displayed
+	 */
 	public Clock(Vector2f position) {
 		super(position, "0.0s");
 	}
-	
+
 	@Override
 	public void update(GameContainer container, StateBasedGame game, GameState<?> state, int delta) {
 		timePassed += delta / 1000.0f;
--- src/de/tudarmstadt/informatik/fop/breakout/gui/Label.java
+++ src/de/tudarmstadt/informatik/fop/breakout/gui/Label.java
@@ -8,19 +8,38 @@ import org.newdawn.slick.state.StateBasedGame;
 import de.tudarmstadt.informatik.fop.breakout.states.GameState;
 import gameObjects.GameObject;
 
+/**
+ * A Label displaying text on screen
+ * 
+ * @author Simon Kohaut
+ */
 public class Label extends GameObject {
 
 	private String text;
-	
+
+	/**
+	 * Create a new Label instance
+	 * 
+	 * @param position
+	 *            The position where the Label will be displayed
+	 * @param text
+	 *            The text to display
+	 */
 	public Label(Vector2f position, String text) {
 		super(position, 0, 0, false);
 		this.text = text;
 	}
-	
+
+	/**
+	 * Set the text the Label will display
+	 * 
+	 * @param text
+	 *            The Text to be displayed
+	 */
 	void setText(String text) {
 		this.text = text;
 	}
-	
+
 	@Override
 	public void update(GameContainer container, StateBasedGame game, GameState<?> state, int delta) {
 	}
